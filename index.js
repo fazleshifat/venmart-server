a91728764a5bedad117dd34d36e190da1906c6c7
fazleshifat <mailbox189.shifat@gmail.com>
2025-06-10 02:21:33 +0600

    set enviroment variable to secure the database key

--- index.js
+++ index.js
@@ -12,11 +12,8 @@ app.use(cors({
 }));
 app.use(express.json());
 
-// venmart_admin
-// EOzNqDj97GNfUGz7
 
-
-const uri = "mongodb+srv://venmart_admin:EOzNqDj97GNfUGz7@cluster0.knw8z6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.knw8z6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
